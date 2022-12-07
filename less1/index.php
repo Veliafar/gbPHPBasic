dea73d8b0d666a2fd6e669050483c840e35efcbc
Artyflex <denis.seo@nxt.ru>
2022-12-07 11:13:16 +0300

    lesson1

--- less1/index.php
+++ less1/index.php
@@ -5,17 +5,18 @@ echo PHP_EOL;
 $name = readline("Ваше имя? ");
 
 $task1 = readline("Какая задача стоит перед вами сегодня? ");
-$task1Time = readline("Сколько примерно времени эта задача займет? ");
+$task1Time = readline("Сколько примерно времени эта задача займет в часах? ");
 
 $task2 = readline("Какая еще задача стоит перед вами сегодня? ");
-$task2Time = readline("Сколько примерно времени эта задача займет? ");
+$task2Time = readline("Сколько примерно времени эта задача займет в часах? ");
 
 $task3 = readline("Третья задача стоит перед вами сегодня? ");
-$task3Time = readline("Сколько примерно времени эта задача займет? ");
+$task3Time = readline("Сколько примерно времени эта задача займет в часах? ");
 
 
 echo PHP_EOL . "{$name}, сегодня у вас запланировано 3 приоритетных задачи на день: " . PHP_EOL;
-echo "- {$task1} ({$task1Time})" . PHP_EOL;
-echo "- {$task2} ({$task2Time})" . PHP_EOL;
-echo "- {$task3} ({$task3Time})" . PHP_EOL;
+echo "- {$task1} ({$task1Time}ч)" . PHP_EOL;
+echo "- {$task2} ({$task2Time}ч)" . PHP_EOL;
+echo "- {$task3} ({$task3Time}ч)" . PHP_EOL;
+echo "Примерное время выолнения плана: {$task1Time + $task2Time + $task3Time}ч" . PHP_EOL;
 
