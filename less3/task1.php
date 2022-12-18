58176fe64d8649f66d8a1e02750cefddf05d7cda
Artyflex <denis.seo@nxt.ru>
2022-12-18 13:25:21 +0300

    lesson3 task1

--- less3/task1.php
+++ less3/task1.php
@@ -1,5 +1,7 @@
 <?php
 
+echo 'Вариант 1' . '  --------------------' . PHP_EOL;
+
 $arr1 = [];
 $arr2 = [];
 $multiplyArrResult = [];
@@ -12,6 +14,30 @@ for ($i = 0; $i < 10; $i++) {
 }
 
 print_r($multiplyArrResult);
+echo PHP_EOL;
+
+
+echo 'Вариант 2' . '  --------------------' . PHP_EOL;
+
+$type2Arr1 = range(0, 100);
+$type2Arr2 = range(0, 100);
+
+shuffle($type2Arr1 );
+shuffle($type2Arr2 );
+
+$type2Arr1 = array_slice($type2Arr1, 0, 10);
+$type2Arr2 = array_slice($type2Arr2, 0, 10);
+
+$type2MultiplyArrResult = range(0, 9);
+
+foreach ($type2MultiplyArrResult as $key => $value) {
+    $type2MultiplyArrResult[$key] = $type2Arr1[$key] * $type2Arr2[$key];
+}
+
+print_r($type2MultiplyArrResult);
+echo PHP_EOL;
+
+
 
 
 
