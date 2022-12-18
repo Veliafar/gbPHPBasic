6a60ca793c127883b3350e4f1a05786b0976b166
Artyflex <denis.seo@nxt.ru>
2022-12-18 13:55:11 +0300

    lesson3 task2

---
+++ less3/task2.php
@@ -0,0 +1,47 @@
+<?php
+
+$wishes = [
+    'счастья',
+    'здоровья',
+    'ума',
+    'потенциала',
+    'процветания',
+    'духа',
+    'слуха',
+    'отваги',
+    'успеха',
+    'веселья',
+];
+
+$epithets = [
+    'крепкого',
+    'сильного',
+    'стойкого',
+    'безудержного',
+    'нереального',
+    'космического',
+    'великолепного',
+    'добротного',
+    'изумительного',
+    'роскошного',
+    'отличного',
+];
+
+echo PHP_EOL;
+
+$name = readline("Имя именинника? ") ?: "Человек";
+
+$epithetsRandomKeys = array_rand($epithets, 3);
+$wishesRandomKeys = array_rand($wishes, 3);
+
+$generatedWishes = range(0, 2);
+
+foreach ($generatedWishes as $i => &$value) {
+    $value = $epithets[$epithetsRandomKeys[$i]] . " " . $wishes[$wishesRandomKeys[$i]];
+}
+
+print_r($generatedWishes);
+
+
+echo PHP_EOL . "Дорогой {$name}, от всего сердца поздравляю тебя с днем рождения, желаю {$generatedWishes[0]}, {$generatedWishes[1]} и {$generatedWishes[2]}!" . PHP_EOL;
+
